c32574d94ba4451abb99cc72e560a1c82aa73ee2
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-06-19 04:19:12 -0400

    Create encryption_utils.h

---
+++ encryption_utils.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <openssl/sha.h>
+#include <string>
+#include <sstream>
+#include <iomanip>
+
+std::string sha256(const std::string& data) {
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256(reinterpret_cast<const unsigned char*>(data.c_str()), data.size(), hash);
+    std::ostringstream result;
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; ++i)
+        result << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+    return result.str();
+}
