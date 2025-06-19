98fdbd61e9753d813f55d010aa37604b84159d41
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-06-19 05:02:25 -0400

    Create Example_Session.sh

---
+++ Example_Session.sh
@@ -0,0 +1,10 @@
+🧠 Enter your function intent:
+> play intro cutscene
+
+👇 Generated Function:
+func intro_cutscene
+    print "Loading intro..."
+    camera move_to 0 0 10 over 3s
+    camera focus entity_hero
+    camera shake 0.2 over 1.5s
+endfunc
